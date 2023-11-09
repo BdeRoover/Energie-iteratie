ac5306a92ec3309e0fab24a95e24e56e87f1ae2c
BdeRoover <bderoover@deklerkbv.nl>
2023-11-09 15:43:12 +0100

    Versie 1.3

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import streamlit as st
 import numpy as np
 
-st.set_page_config(page_title="Energieiteratie", layout="centered")
+st.set_page_config(
+    page_title="Energieiteratie",
+    layout="centered"
+    )
 
-  
-col1, col2, col3 = st.columns([1,1,3])
+col1, col2, col3 = st.columns([1,1,2])
 
-with col1:
+with col1:    
     st.header('Input')
-    Eben = st.number_input("Benodigde energie [kNm]") or 1
-
-    F = st.number_input("Stootkracht [kN]") or 1
-    U = st.number_input("Vervorming [mm]") or 1
-    Eopt = round( 0.5*F*(U/1000),2)
+    Eben = st.number_input("Benodigde energie [kNm]", min_value=1.00)    
+    F = st.number_input("Stootkracht [kN]", min_value=10.00)
+    U = st.number_input("Vervorming [mm]", min_value=10.00)
+    Eopt = round(0.5*F*(U/1000),2)
 
 with col2:
     st.empty()
@@ -23,16 +24,16 @@ with col3:
     f = round(Eopt / Eben,3)
 
     if Eopt < Eben:
-        st.write(f"De optredende energie bedraagt :red[{Eopt}] [kNm]")
+        st.write(f"De optredende energie bedraagt 0.5 x {F} x {U/1000} = :red[{Eopt}] [kNm]")
     else:
-         st.write(f"De optredende energie bedraagt :green[{Eopt}] [kNm]")   
+         st.write(f"De optredende energie bedraagt 0.5 x {F} x {U/1000} = :green[{Eopt}] [kNm]")   
             
     # st.write("De veerwaarde bedraagt: ", K, '[kN/m]')
     
     if 1 < f < 1.02:
-        st.write(f"De afwijking bedraagt :green[{f}] [-]")
+        st.write(f"De afwijking bedraagt {Eopt}/{Eben} = :green[{f}] [-]")
     else:
-        st.write(f"De afwijking bedraagt :red[{f}] [-]")
+        st.write(f"De afwijking bedraagt {Eopt}/{Eben} = :red[{f}] [-]")
     
     Fs = round(np.sqrt(Eben/Eopt)*F,2)
-    st.write(f"De nieuwe stootkracht bedraagt :black[{Fs}] [kN]")
+    st.write(f"De nieuwe stootkracht bedraagt \u221A({Eben}/{Eopt}) x {F} =  :black[{Fs}] [kN]")
